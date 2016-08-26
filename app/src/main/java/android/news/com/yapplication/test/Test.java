472c4028f69afa2beecaeec3bfb10eb37eb1ae89
yueyang <10017@ly.com>
2016-08-26 10:25:07 +0800

    update code

--- app/src/main/java/android/news/com/yapplication/test/Test.java
+++ app/src/main/java/android/news/com/yapplication/test/Test.java
@@ -1,5 +1,9 @@
 package android.news.com.yapplication.test;
 
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
  * Created by yy10017 on 2016/8/22.
  * 这个类用来测试
@@ -7,7 +11,21 @@ package android.news.com.yapplication.test;
 
 public class Test {
 
+    public static final int INT = 16;
+
     public static void main(String []args){
-        // TODO: 2016/8/22  
-    }
+        // TODO: 2016/8/22
+        Map<String, String> map = new LinkedHashMap<>(16,0.75f,true);
+        map.put("1", "a");
+        map.put("2", "b");
+        map.put("3", "c");
+        map.put("4", "e");
+
+        map.get("1");
+        map.get("2");
+
+        for (Iterator<String> iterator = map.values().iterator(); iterator.hasNext();) {
+            String name = iterator.next();
+            System.out.print(name);
+        }}
 }
