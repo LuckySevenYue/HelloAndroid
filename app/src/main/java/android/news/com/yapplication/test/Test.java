0cae66772f9d2fc51a23ab6db1a699a89bdf492b
yueyang <10017@ly.com>
2016-08-30 17:20:07 +0800

    Test.java file  change  for  test.

--- app/src/main/java/android/news/com/yapplication/test/Test.java
+++ app/src/main/java/android/news/com/yapplication/test/Test.java
@@ -8,6 +8,27 @@ package android.news.com.yapplication.test;
 public class Test {
 
     public static void main(String []args){
-        // TODO: 2016/8/22  
+        // TODO: 2016/8/22
+        String str = "0  1 2 3 5";
+        /**************fun1()**********************/
+        String newStr = "";
+        String arr[] = str.split(" ");
+        for (int i = 0; i < arr.length; i++) {
+            newStr += arr[i];
+        }
+        System.out.println("s1 = " + newStr);
+        /**************fun2()**********************/
+        String ss = "";
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == ' '){
+                continue;
+            }
+            ss += Character.toString(c);
+        }
+        System.out.println("s2 = " + ss);
+
+        /**************fun3()**********************/
+        System.out.println("s3 = " + str.replace(" ",""));
     }
 }
